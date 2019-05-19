a005e044ebeab6f48d7084ab9310be5174f440c9
Jana Uhrich <janamu@uw.edu>
2019-05-18 23:46:30 -0700

    trying to get tests to work

--- ecommerce-ui/src/App.test.js
+++ ecommerce-ui/src/App.test.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
 import { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-
+import airbnbJSON from "./_data/airbnbs.json";
 import App from './App';
 import AirbnbList from "./airbnbList";
-import AirbnbButton from "./AirbnbButton";
-import ShoppingCart from "./ShoppingCart";
+
+const testData = airbnbJSON;
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -18,14 +17,17 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('should match snapshot', () => { 
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
 
 describe('<AirbnbList />', () => {
   it('allows us to set props', () => {
-    const a = [1,2,3];
-    const wrapper = mount(<AirbnbList airbnbs={[a]} />);
-    expect(wrapper.props().airbnbs).to.equal('[1,2,3]');
-    wrapper.setProps({ airbnbs: 'AirbnbList' });
-    expect(wrapper.props().airbnbs).to.equal('AirbnbList');
+    const wrapper = mount(<AirbnbList airbnbs={testData} onBookAirbnb={testData[0]}/>);
+    expect(wrapper.props().airbnbs).to.equal('{testData}');
+    //wrapper.setProps({ airbnbs: 'AirbnbList' });
+    //expect(wrapper.props().airbnbs).to.equal('AirbnbList');
   });
 
 
